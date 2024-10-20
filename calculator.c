7adf5e7b324da48a644010412e0eaabc6164b1dd
Jasmin Lorino <lorinojt@students.national-u.edu.ph>
2024-10-20 20:53:36 +0800

    Added modulo feature.

--- calculator.c
+++ calculator.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int firstNum, secondNum, sumNum, diffNum, productNum, quotientNum;
+    int firstNum, secondNum, sumNum, diffNum, productNum, quotientNum, moduloNum;
 
     printf("Enter first number: ");
     scanf("%d", &firstNum);
@@ -14,9 +14,13 @@ int main()
     diffNum = firstNum - secondNum;
     productNum  = firstNum * secondNum;
     quotientNum = firstNum / secondNum;
+    moduloNum = firstNum % secondNum;
 
     printf("%d + %d = %d", firstNum, secondNum, sumNum);
     printf("\n%d - %d = %d", firstNum, secondNum, diffNum);
     printf("\n%d x %d = %d", firstNum, secondNum, productNum);
     printf("\n%d / %d = %d", firstNum, secondNum, quotientNum);
+    printf("\n%d %% %d = %d", firstNum, secondNum, moduloNum);
+
+    return 0;
 }
